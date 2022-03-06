c5cc686e88b35f18c62c5710c5b1f211e3a090ae
Nathalia Souza <dasilvasouzanathalia@gmail.com>
2022-03-06 12:41:36 -0300

    Interação com o menu

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".n-container");
+const toggle = document.querySelectorAll(".toggle");
+
+for (const element of toggle) {
+    element.addEventListener('click', function () {
+        nav.classList.toggle('show')
+    })
+    
+}
+
+const links = document.querySelectorAll(".menu ul li a");
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show');
+    })
+}
